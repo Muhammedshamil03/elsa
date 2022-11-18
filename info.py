ea3bacc253ce538744e9e6c3a23e3f95cf6ebe0f
Muhammed Shamil <105405467+Muhammedshamil03@users.noreply.github.com>
2022-11-18 13:09:23 +0530

    Update info.py

--- info.py
+++ info.py
@@ -46,7 +46,7 @@ BOT_TOKEN = environ['BOT_TOKEN']
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
 PICS = (environ.get('PICS', 'https://telegra.ph/file/2992a480cae2bc0de1c39.jpg https://telegra.ph/file/76e7b5e94430b84a3d2b2.jpg https://telegra.ph/file/3544a8773740b0412c9dd.jpg https://telegra.ph/file/4b1c7004ea8bd3fed8df9.jpg https://telegra.ph/file/a02e47d932adc336740fa.jpg')).split()
-NOR_IMG = environ.get("NOR_IMG", "https://telegra.ph/file/0662b9b62f452a45c5cde.jpg")
+NOR_IMG = environ.get("NOR_IMG", "CAACAgUAAxkBAAEDFFJjdzZPC8SdNRrI5Bf7ievvZwTosAACSgUAAp3vmVaNvfNFRXF4eB4E")
 
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
